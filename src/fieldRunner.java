b4f9a13b82f54b0113fd4546a3ab8f58530c4c42
coopereisman <cooper@eisman.me>
2022-01-21 20:19:20 -0700

    Begin working on text prompt

--- src/fieldRunner.java
+++ src/fieldRunner.java
@@ -2,12 +2,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class fieldRunner {
-    ArrayList<String> itemNames = new ArrayList<String>();
-    ArrayList<fieldItem> items = new ArrayList<fieldItem>();
-
+    private ArrayList<String> itemNames = new ArrayList<String>();
+    private ArrayList<fieldItem> items = new ArrayList<fieldItem>();
+    private fieldConfig config = new fieldConfig();
+    private boolean shouldDie = false;
+    private Scanner console = new Scanner(System.in);
+    private String command;
 
     public fieldRunner() {
-        System.out.println("What would you like to do?");
+        command = "";
+
+        while(!shouldDie) {
+            printMenu();
+            command = console.nextLine();
+        }
+    }
+
+    public void printMenu() {
+        System.out.println("===========================");
+        System.out.println("What would you like to do?: ");
+        System.out.println(" - Add <Vector/Point>");
+        System.out.println(" - Remove <Item Name>");
+        System.out.println(" - Edit <Item Name>");
+        System.out.println(" - Compute");
+        System.out.println("===========================");
     }
 
 
--- src/main.java
+++ src/main.java
@@ -1,14 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        double[] a = {3.0,-2.0,7.0};
-        double[] b = {3.0,2.0,1.0};
-        fieldVector VectorOne = new fieldVector(a);
-        fieldConfig f = new fieldConfig();
-        System.out.println(f);
-
-
-        System.out.println(f.addItem(VectorOne,"VectorOne"));
-        System.out.println(f);
+        fieldRunner fr = new fieldRunner();
 
     }
 
